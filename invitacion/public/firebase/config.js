6d8a6c035b0e73d8302fad2a546d1d8fb9d1294b
carolainseija <carolainsilva1@gmail.com>
2023-09-29 13:26:20 -0300

    opcion 2

---
+++ invitacion/public/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTKWejaH0aMGiIOYTiDQMekC9AVRZNBJc",
+  authDomain: "invitacion2-7b24f.firebaseapp.com",
+  projectId: "invitacion2-7b24f",
+  storageBucket: "invitacion2-7b24f.appspot.com",
+  messagingSenderId: "466451991814",
+  appId: "1:466451991814:web:57f0cc0b3a3875ec552287"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
